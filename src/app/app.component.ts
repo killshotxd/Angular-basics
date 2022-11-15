0d4ba1f4a8ecf30986e143e77271f64d905e6fa4
killshotxd <hassanansari211@gmail.com>
2022-11-15 17:58:05 +0530

    custom events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,4 +19,7 @@ export class AppComponent {
   changeImage(e: KeyboardEvent) {
     this.imgURL = (e.target as HTMLInputElement).value;
   }
+  logImg(event: string) {
+    console.log(event);
+  }
 }
