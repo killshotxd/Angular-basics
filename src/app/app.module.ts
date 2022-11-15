cf840856494a4d25745473b9ecce8a259316954b
killshotxd <hassanansari211@gmail.com>
2022-11-15 17:44:27 +0530

    generating component with CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, PostComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
