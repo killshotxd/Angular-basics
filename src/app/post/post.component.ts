0a3bf46725889d1cbd872b1615f342b83828f63f
killshotxd <hassanansari211@gmail.com>
2022-11-15 17:50:02 +0530

    custom properties

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css'],
 })
-export class PostComponent {}
+export class PostComponent {
+  @Input() postImg = '';
+}
