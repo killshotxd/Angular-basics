bb039ff4cf5717ec777c5b82f41d48f570feb585
killshotxd <hassanansari211@gmail.com>
2022-11-15 18:11:40 +0530

    Update post.component.ts

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,11 +1,19 @@
-import { Component, Input, EventEmitter, Output, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  EventEmitter,
+  Output,
+  OnInit,
+  OnChanges,
+  DoCheck,
+} from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css'],
 })
-export class PostComponent implements OnInit {
+export class PostComponent implements OnInit, OnChanges, DoCheck {
   @Input('img') postImg = '';
   @Output() imgSelected = new EventEmitter<string>();
 
@@ -16,4 +24,14 @@ export class PostComponent implements OnInit {
   ngOnInit() {
     console.log('ngOnInit() called...', this.postImg);
   }
+
+  ngDoCheck() {
+    console.log('ngDoCheck() called.....');
+  }
+
+  ngOnChanges() {
+    console.log('ngOnChanges() called.....');
+  }
 }
+
+// 5:39 11 (04)
