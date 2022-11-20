de280faaffd4681225d91351c9e96e36c77d7e5b
killshotxd <hassanansari211@gmail.com>
2022-11-20 10:36:08 +0530

    titlecase pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
   // styles:[] INLINE STYLES
 })
 export class AppComponent {
-  name = 'killshot';
+  name = 'killshot bruh';
   imgURL =
     'https://images.unsplash.com/photo-1668503714845-e5d67cc94f05?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60';
 
